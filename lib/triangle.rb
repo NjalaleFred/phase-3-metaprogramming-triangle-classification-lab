193f847e7b3478ac23e00fa6841379b0c53679a0
Njalale <fnjalale@gmail.com>
2023-05-22 21:21:50 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  def initialize(base,height,hypotenuse)
+    @base = base
+    @height = height
+    @hypotenuse = hypotenuse
+    validate_triangle
+  end
+
+  def kind
+    if @base == @height && @base == @hypotenuse
+      :equilateral
+    elsif @base == @height || @base == @hypotenuse || @height == @hypotenuse
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
+
+  private
+  def validate_triangle
+    if @base == 0 && @height == 0 && @hypotenuse == 0
+      raise TriangleError
+    elsif @base < 0 || @height < 0 || @hypotenuse < 0
+      raise TriangleError
+    elsif @base + @height <= @hypotenuse || @base + @hypotenuse <= @height || @height + @hypotenuse <= @base
+      raise TriangleError
+    end
+  end
+  
 end
